4bcb1c6fcc1068035547b4f15b0d970c22ad5597
Gianluca Scorzoni <gscorzon@c2r1s2.42roma.it>
2026-07-02 17:19:42 +0200

    failed to implement a status control for when everything has been read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gscorzon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 10:01:21 by gscorzon          #+#    #+#             */
-/*   Updated: 2026/07/02 17:12:23 by gscorzon         ###   ########.fr       */
+/*   Updated: 2026/07/02 17:18:16 by gscorzon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*get_next_line(int fd)
 {
 	static char	*buf = NULL;
 	char		*line;
+	int 		found;
 	
 	if (!buf)
 	{
@@ -24,12 +25,14 @@ char	*get_next_line(int fd)
 				return (NULL);
 		buf[BUFFER_SIZE] = 0;
 	}
+	found = read(fd, buf, BUFFER_SIZE); 
 	if (!*buf)
-		line = gnl(fd, buf, NULL, read(fd, buf, BUFFER_SIZE));
+		line = gnl(fd, buf, NULL, found);
 	line = gnl(fd, buf, NULL, 1);
 	if (line == NULL && buf)
 	{
 		free(buf);
+		buf = found;
 		return (NULL);
 	}
 	return (line);
