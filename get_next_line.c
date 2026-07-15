ce3a8258e83af3b96ea144033bd1332464e51605
Gianluca Scorzoni <scorzonigianluca@gmail.com>
2026-07-15 20:16:10 +0200

    real first commit
    might have committed empty repo

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+char	*get_next_line(int fd)
+{
+	static char	*buf = NULL;
+	static char	*stash = NULL;
+	int			status;
+	
+	if (!buf)
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		buf[BUFFER_SIZE] = 0;
+	}
+	status = read(fd, buf, BUFFER_SIZE);
+	if (status <= 0)
+		return (stash);// WARNING: if stash is empty it must also be free'd and set to NULL;
+	ptr = ft_strchr(buf, '\n');
+	if (ptr)
+		line = ft_nl_handler(buf, stash, ptr);// if there's a nl, ft_nl_handler handles it (nl at end or middle)
+	else// if there is no nl
+	{
+		if (stash)
+			ft_strjoin(stash, buf);// if there is content stashed, join it with current buf
+		else
+			ft_arrfill(stash, buf, ft_strchr(buf, 0)):// if stash is empty, fill it with current buf content
+	}
+	return (line);
+}
+/***********************
+ * FUNCTIONS TO WRITE
+ * 	ft_strchr
+ * 	ft_nl_handler
+ * 	ft_strjoin
+ * 	ft_arrfill	
+ ***********************
